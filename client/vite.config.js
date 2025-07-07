8abd0be15cdec720f908958c554db41f5d09621f
satakratu6 <satakratu.madhu@gmail.com>
2025-07-07 15:12:40 +0530

    fix: alias @mui/icons-material to resolve Rollup failure

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@mui/icons-material': path.resolve(__dirname, 'node_modules/@mui/icons-material'),
+    },
+  },
   build: {
     rollupOptions: {
       external: ['fs', 'path', 'os', 'crypto']
